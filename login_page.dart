95dea52b4719339b217e57e42ab99a3dd7aceecc
AnmolGanorkar <85453451+AnmolGanorkar@users.noreply.github.com>
2023-04-01 21:12:59 +0530

    Create login_page.dart

---
+++ login_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isStudent = true;
+
+  void toggleLoginOption() {
+    setState(() {
+      isStudent = !isStudent;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BUS TRACKER'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              isStudent ? 'STUDENT LOGIN' : 'DRIVER LOGIN',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Log In'),
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: toggleLoginOption,
+              child: Text(isStudent ? 'Log in as Driver' : 'Log in as Student'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
