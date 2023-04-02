bb749830cd9694eb35b5a1989abbbacdbdb359b7
Kapoor-0905 <500097111@stu.upes.ac.in>
2023-04-02 19:16:52 +0530

    Folders added ; Flutter boilerplate added

--- main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:my_project/screen/locations/locations_page.dart';
-
-void main() {
-  runApp(MaterialApp(
-    title: 'Bus Tracking App',
-    home: LocationsPage(),
-  ));
-}
