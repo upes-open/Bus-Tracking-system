897b92911f1238179ae62b69cdc7f569f7c338f4
Rishabh Verma <114498105+rishabh-108272@users.noreply.github.com>
2023-04-03 09:12:26 +0530

    routes page-1

---
+++ src/bus_tracking_system/lib/routes_page_1.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:my_project/screen/locations/locations_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Bus Tracking App',
+    home: LocationsPage(),
+  ));
+}
