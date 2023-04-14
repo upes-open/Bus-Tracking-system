f38d5bc438ae6f775ebf4a1f3dd4ef2344f99a9b
Rishabh Verma <114498105+rishabh-108272@users.noreply.github.com>
2023-04-14 20:05:17 +0530

    locations_page
    Improved button style and added scroll feature

--- src/bus_tracking_system/lib/screen/locations/locations_page.dart
+++ src/bus_tracking_system/lib/screen/locations/locations_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:bus_tracking_system/screen/locations/components/mapPage.dart';
+import 'package:bus_tracking_system/screen/locations/components/map_page.dart';
 
 class LocationsPage extends StatefulWidget {
   @override
@@ -11,79 +11,101 @@ class _LocationsPageState extends State<LocationsPage> {
     'Prem Nagar UPES',
     'Clock Tower',
     'Dehradun ISBT',
-    'Bidholi'
+    'Bidholi',
+    'Kandholi',
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Select Route'),
+        backgroundColor: Colors.white,
+        title: Text(
+          'Select Route',
+          style: TextStyle(color: Colors.black),
+        ),
       ),
-      body: ListView.builder(
-        itemCount: _locations.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Prem Nagar UPES',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.grey[600],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: _locations.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  child: Card(
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            _locations[index],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            'Driver: John Doe',
+                            style: TextStyle(
+                                fontWeight: FontWeight.normal, fontSize: 12),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            'Phone: +91-7894521642',
+                            style: TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.normal),
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '',
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  // Navigate to the map page for the selected location
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          MapPage(location: _locations[index]),
+                                    ),
+                                  );
+                                },
+                                child: Text(
+                                  'Track in Map',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.pink,
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 50, vertical: 15),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Driver: John Doe',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    'Phone: +91-7894521642',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          // Navigate to the map page for the selected location
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  MapPage(location: _locations[index]),
-                            ),
-                          );
-                        },
-                        child: Text(
-                          'Track in Map',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.pink),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                );
+              },
             ),
-          );
-        },
+          ],
+        ),
       ),
     );
   }
