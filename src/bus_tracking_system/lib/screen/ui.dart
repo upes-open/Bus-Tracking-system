ca512b394ef8b347589678a08c9bb4d692e62ce7
prathamkandari <prathamkandari123@gmail.com>
2023-04-23 00:38:09 +0530

    Login Page Completed

--- src/bus_tracking_system/lib/screen/ui.dart
+++ src/bus_tracking_system/lib/screen/ui.dart
@@ -1,29 +1,31 @@
+import 'dart:math';
+import 'package:bus_tracking_system/Constants/constants.dart';
 import 'package:bus_tracking_system/helper/helperFunction.dart';
+import 'package:bus_tracking_system/screen/splash.dart';
 import 'package:bus_tracking_system/services/authServices.dart';
 import 'package:bus_tracking_system/services/databaseServices.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:bus_tracking_system/screen/locations/locations_page.dart';
-=======
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
->>>>>>> 1539f06958eafa40aa369b40ee2ddb279aabf01b
 
-
-class UI extends StatefulWidget {//is a 
+class UI extends StatefulWidget {
+  //is a
   @override
-  _UIState createState() => _UIState();
+  State<UI> createState() => _UIState();
 }
 
 class _UIState extends State<UI> {
-  AuthService _auth = AuthService()
-  
-  
+  AuthService _auth = AuthService();
   bool isStudent = true;
   late final String email;
   late final String password;
+  final _formfield = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
+  bool passToggle = true;
 
   void toggleLoginOption() {
     setState(() {
@@ -36,60 +38,123 @@ class _UIState extends State<UI> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('BUS TRACKER',),
+        title: Text(
+          'BUS TRACKER',
+        ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              isStudent ? 'STUDENT LOGIN' : 'DRIVER LOGIN',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+            child: Form(
+                key: _formfield,
+                child: SingleChildScrollView(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  isStudent ? 'STUDENT LOGIN' : 'DRIVER LOGIN',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                  validator: (value) {
+                    bool emailValid = RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-z0-9]+\.[a-zA-Z]+")
+                        .hasMatch(value!);
+
+                    if (value.isEmpty) {
+                      return "Enter Email";
+                    } else if (!emailValid) {
+                      return "Enter valid Email";
+                    }
+                  },
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: passController,
+                  obscureText: passToggle,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: InkWell(
+                        onTap: () {
+                          setState(() {
+                            passToggle = !passToggle;
+                          });
+                        },
+                        child: Icon(passToggle
+                            ? Icons.visibility_off
+                            : Icons.visibility)),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Enter Password';
+                    } else if (passController.text.length < 9) {
+                      return "Password length should be more than 9 characters";
+                    }
+                  },
+                ),
+                SizedBox(height: 20),
+                InkWell(
+                  onTap: () {
+                    if (_formfield.currentState!.validate()) {
+                      print("Login Successful");
+                      emailController.clear();
+                      passController.clear();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return LocationsPage();
+                          },
+                        ),
+                      );
+                    }
+                  },
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                
+                SizedBox(height: 10),
+                TextButton(
+                  onPressed: toggleLoginOption,
+                  child: Text(
+                    isStudent ? 'Log in as Driver' : 'Log in as Student',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            ))
             ),
-            SizedBox(height: 20),
-            TextField(
-          decoration: InputDecoration(
-              hintText: "Email",
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(18),
-                  borderSide: BorderSide.none),
-              fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
-              filled: true,
-              prefixIcon: Icon(Icons.person)),
-        ),
-        SizedBox(height: 10),
-        TextField(
-          decoration: InputDecoration(
-            hintText: "Password",
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(18),
-                borderSide: BorderSide.none),
-            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
-            filled: true,
-            prefixIcon: Icon(Icons.person),
           ),
-          obscureText: true,
-        ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Login', style: TextStyle(fontSize: 20),
-              ),
-              style: ElevatedButton.styleFrom(
-            shape: StadiumBorder(),
-            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 67),
-          ),
-            ),
-            SizedBox(height: 10),
-            TextButton(
-              onPressed: toggleLoginOption,
-              child: Text(isStudent ? 'Log in as Driver' : 'Log in as Student', style: TextStyle(fontSize: 20),),
-            ),
-          ],
-        ),
       ),
     );
   }
@@ -100,22 +165,19 @@ class _UIState extends State<UI> {
     properties.add(StringProperty('password', password));
   }
 
-  login() async{
-    await _auth.loginWithUserEmailandPassword(email, password).then((value) async{//auth services instance
-      if(value==true){
-        QuerySnapshot snapshot= await DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid).getUserData( email) ;
-    //sharedrefences
-    await HelperFunctions.savedUserLoggedInStatus(true);
-    await HelperFunctions.savedUserEmailSF(email);
-    //await HelperFunctions.getUserNamefromSF(true);
-   /* Navigator.push(context,MaterialPageRoute(builder:(context) {
-      homepage
-    }))*/
-
-
+  login() async {
+    await _auth
+        .loginWithUserEmailandPassword(email, password)
+        .then((value) async {
+      //auth services instance
+      if (value == true) {
+        QuerySnapshot snapshot =
+            await DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid)
+                .getUserData(email);
+        //sharedrefences
+        await HelperFunctions.savedUserLoggedInStatus(true);
+        await HelperFunctions.savedUserEmailSF(email);
       }
-    });//
-    
-
+    });
   }
 }
--- src/bus_tracking_system/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/bus_tracking_system/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,13 @@ import Foundation
 import cloud_firestore
 import firebase_auth
 import firebase_core
+import location
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
