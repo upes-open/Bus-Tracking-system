c0d8f7a90d92ed034ade9d76ba78eff0681ee035
Rishabh Verma <114498105+rishabh-108272@users.noreply.github.com>
2023-04-23 00:31:20 +0530

    main.dart

--- main.dart
+++ main.dart
@@ -1,9 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:my_project/screen/locations/locations_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Bus Tracking App',
-    home: LocationsPage(),
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return SomethingWentWrong();
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Bus Tracking App',
+            home: LocationsPage(),
+          );
+        }
+
+        return Loading();
+      },
+    );
+  }
+}
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
+class SomethingWentWrong extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Something went wrong.',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
 }
