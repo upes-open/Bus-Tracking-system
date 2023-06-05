03c2645f046b443e6f642df033a895f3b63def67
unknown <siddharthaarora2131@gmail.com>
2023-06-05 23:51:26 +0530

    changed the color

--- src/bus_tracking_system/lib/screen/ui.dart
+++ src/bus_tracking_system/lib/screen/ui.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:bus_tracking_system/screen/locations_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 
 class UI extends StatefulWidget {
   //is a
@@ -42,116 +43,138 @@ class _UIState extends State<UI> {
           'BUS TRACKER',
         ),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 60),
-          child: Form(
-              key: _formfield,
-              child: SingleChildScrollView(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  /*Text(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(children: <Widget>[
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                    Color(0x6645b6fe),
+                    Color(0x9945b6fe),
+                    Color(0xcc45b6fe),
+                    Color(0xff45b6fe),
+                  ])),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+                child: Form(
+                    key: _formfield,
+                    child: SingleChildScrollView(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        /*Text(
                     /*isStudent ? "" : "",
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                     ),*/
                   ),*/
-                  SizedBox(height: 20),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      labelText: isStudent ? " Student@domain" : "driver@email",
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.email),
-                    ),
-                    validator: (value) {
-                      bool emailValid = RegExp(
-                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-z0-9]+\.[a-zA-Z]+")
-                          .hasMatch(value!);
-
-                      if (value.isEmpty) {
-                        return "Enter Email";
-                      } else if (!emailValid) {
-                        return "Enter valid Email";
-                      }
-                    },
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    controller: passController,
-                    obscureText: passToggle,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.lock),
-                      suffixIcon: InkWell(
-                          onTap: () {
-                            setState(() {
-                              passToggle = !passToggle;
-                            });
-                          },
-                          child: Icon(passToggle
-                              ? Icons.visibility_off
-                              : Icons.visibility)),
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Enter Password';
-                      } else if (passController.text.length < 9) {
-                        return "Password length should be more than 9 characters";
-                      }
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  InkWell(
-                    onTap: () {
-                      if (_formfield.currentState!.validate()) {
-                        print("Login Successful");
-                        emailController.clear();
-                        passController.clear();
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (BuildContext context) {
-                              return LocationsPage();
-                            },
+                        SizedBox(height: 20),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            labelText:
+                                isStudent ? " Student@domain" : "driver@email",
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.email),
                           ),
-                        );
-                      }
-                    },
-                    child: Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.blueAccent,
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                          validator: (value) {
+                            bool emailValid = RegExp(
+                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-z0-9]+\.[a-zA-Z]+")
+                                .hasMatch(value!);
+
+                            if (value.isEmpty) {
+                              return "Enter Email";
+                            } else if (!emailValid) {
+                              return "Enter valid Email";
+                            }
+                          },
+                        ),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: passController,
+                          obscureText: passToggle,
+                          decoration: InputDecoration(
+                            labelText: "Password",
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.lock),
+                            suffixIcon: InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    passToggle = !passToggle;
+                                  });
+                                },
+                                child: Icon(passToggle
+                                    ? Icons.visibility_off
+                                    : Icons.visibility)),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Enter Password';
+                            } else if (passController.text.length < 9) {
+                              return "Password length should be more than 9 characters";
+                            }
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        InkWell(
+                          onTap: () {
+                            if (_formfield.currentState!.validate()) {
+                              print("Login Successful");
+                              emailController.clear();
+                              passController.clear();
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (BuildContext context) {
+                                    return LocationsPage();
+                                  },
+                                ),
+                              );
+                            }
+                          },
+                          child: Container(
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.blueAccent,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  TextButton(
-                    onPressed: toggleLoginOption,
-                    child: Text(
-                      isStudent ? 'Log in as Driver' : 'Log in as Student',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ],
-              ))),
+                        SizedBox(height: 10),
+                        TextButton(
+                          onPressed: toggleLoginOption,
+                          child: Text(
+                            isStudent
+                                ? 'Log in as Driver'
+                                : 'Log in as Student',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ))),
+              ),
+            )
+          ]),
         ),
       ),
     );
