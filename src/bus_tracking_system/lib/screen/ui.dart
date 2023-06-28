359fb534904b08442ce6eddd6cff982ee73210ec
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-06-28 16:15:17 +0530

    added

--- src/bus_tracking_system/lib/screen/ui.dart
+++ src/bus_tracking_system/lib/screen/ui.dart
@@ -53,7 +53,7 @@ class _UIState extends State<UI> {
               // backgroundColor: Color(0xFFE8F5E9),
 
               SizedBox(height: 100),
-              Text(
+              const Text(
                 'Sign up',
                 style: TextStyle(
                   //color: Color(0xFF34BAC3),
@@ -88,7 +88,7 @@ class _UIState extends State<UI> {
                   hintText: isStudent
                       ? 'Enter student email'
                       : 'Enter bus-driver email',
-                  hintStyle: TextStyle(
+                  hintStyle: const TextStyle(
                     color: Color.fromARGB(169, 106, 196, 207),
                     fontSize: 18.0,
                   ),
@@ -132,7 +132,7 @@ class _UIState extends State<UI> {
                   selectionColor: Colors.blueAccent,
                 ),
               ),
-              SizedBox(height: 150),
+              const SizedBox(height: 150),
               MyButton(
                 label: 'Sign up',
                 onTap: () async {
@@ -165,198 +165,3 @@ class _UIState extends State<UI> {
     );
   }
 }
-/*
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties.add(StringProperty('password', password));
-  }
-
-  login() async {
-    await _auth
-        .loginWithUserEmailandPassword(email, password)
-        .then((value) async {
-      //auth services instance
-      if (value == true) {
-        QuerySnapshot snapshot =
-            await DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid)
-                .getUserData(email);MyButton(
-  label: 'Sign up',
-  onTab: () async {
-    if (_formfield.currentState!.validate()) {
-      String password = passController.text; // Assign password from passController
-      dynamic result = await _auth.loginWithUserEmailandPassword(
-        emailController.text,
-        password,
-      );
-      if (result == null) {
-        // Handle login failure
-      } else {
-        print("Login Successful");
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (BuildContext context) {
-              return LocationsPage();
-            },
-          ),
-        );
-      }
-    }
-  },
-),
-
-        //sharedrefences
-        await HelperFunctions.savedUserLoggedInStatus(true);
-        await HelperFunctions.savedUserEmailSF(email);
-      }
-    });
-  }
-}
-
-
-/*
-
-if (_formfield.currentState!.validate()) {
-                      print("Login Successful");
-                      emailController.clear();
-                      passController.clear();
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) {
-                            return LocationsPage();
-   TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            controller: emailController,
-                            decoration: InputDecoration(
-                              labelText: isStudent
-                                  ? " Student@domain" 
-                                  : "driver@email",
-                              focusColor: Colors.white12,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(color: Colors.white),
-                              ),
-                              prefixIcon: Icon(Icons.email),
-                            ),
-                            validator: (value) {
-                              bool emailValid = RegExp(
-                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-z0-9]+\.[a-zA-Z]+")
-                                  .hasMatch(value!);
-
-                              if (value.isEmpty) {
-                                return "Enter Email";
-                              } else if (!emailValid) {
-                                return "Enter valid Email";
-                              }
-                              return null;
-                            },
-                          ),
-                          SizedBox(height: 10),
-                          TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            controller: passController,
-                            obscureText: passToggle,
-                            decoration: InputDecoration(
-                              labelText: "Password",
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide:
-                                    BorderSide(color: Colors.white),
-                              ),
-                              prefixIcon: Icon(Icons.lock),
-                              suffixIcon: InkWell(
-                                  onTap: () {
-                                    setState(() {
-                                      passToggle = !passToggle;
-                                    });
-                                  },
-                                  child: Icon(passToggle
-                                      ? Icons.visibility_off
-                                      : Icons.visibility)),
-                            ),
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return 'Enter Password';
-                              } else if (passController.text.length < 9) {
-                                return "Password length should be more than 9 characters";
-                              }
-                              return null;
-                            },
-                          ),
-                          SizedBox(height: 50),
-                          InkWell(
-                            onTap: () {
-                              if (_formfield.currentState!.validate()) {
-                                print("Login Successful");
-                                emailController.clear();
-                                passController.clear();
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (BuildContext context) {
-                                      return LocationsPage();
-                                    },
-                                  ),
-                                );
-                              }
-                            },
-                            child: Container(
-                              height: 50,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  "Login",
-                                  style: TextStyle(
-                                    color: Colors.blueAccent,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          TextButton(
-                            onPressed: toggleLoginOption,
-                            child: Text(
-                              isStudent
-                                  ? 'Log in as Driver'
-                                  : 'Log in as Student',
-                              style: TextStyle(fontSize: 20),
-                              selectionColor: Colors.blueAccent,
-                            ),
-                          ),
-
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties.add(StringProperty('password', password));
-  }
-
-  login() async {
-    await _auth
-        .loginWithUserEmailandPassword(email, password)
-        .then((value) async {
-      //auth services instance
-      if (value == true) {
-        QuerySnapshot snapshot =
-            await DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid)
-                .getUserData(email);
-        //sharedrefences
-        await HelperFunctions.savedUserLoggedInStatus(true);
-        await HelperFunctions.savedUserEmailSF(email);
-        )
-    ),
-    );
-      }
-    });
-  }
-}
-*/
-*/
